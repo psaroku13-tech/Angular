63af176407d89018522fd8da7411225afa96bf4b
Saro <psaro.ku13@gmail.com>
2026-08-21 15:20:40 +0530

    basic changes

--- basic.js
+++
@@ -1,8 +0,0 @@
-function getName(name)
-{
-    return `Hello"+ ${name}`;
-}
-
-console.log(getName("Saroj"));
-
-console.log(getName(42));
---
+++ basic.ts
@@ -0,0 +1,28 @@
+/*function getName(name)
+{
+    return `Hello"+ ${name}`;
+}
+
+console.log(getName("Saroj"));
+
+console.log(getName(42));
+*/
+
+
+function getName(name:string) : string
+{
+    return `Hello ${name}`
+}
+
+console.log(getName("good"));
+console.log(42);
+
+//npm init -y  =package.json
+
+
+//npm i -D typescript   --dev dependecy it will add on package.json
+
+// npx ==node package executor
+//tsc =typescript compilor 
+
+//npx tsc --init  -->to create tsconfig file
